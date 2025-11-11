01c623e0aa4ba844e4a63c3f3718ed80219df14d
ethanparker3512 <ethanparker3512@gmail.com>
2025-11-11 16:43:37 +0100

    Added menu data

--- menu.js
+++ menu.js
@@ -1,17 +1,74 @@
-const menu = [
+export const menuData = [
   {
     name: "2 for 1 Special",
-    image: "https://example.com/image1.jpg"
+    image: "https://example.com/images/2for1.jpg"
   },
   {
     name: "Special Offers",
-    image: "https://example.com/image2.jpg"
+    image: "https://example.com/images/specialoffers.jpg"
   },
   {
     name: "Starters",
-    image: "https://example.com/image3.jpg"
+    image: "https://example.com/images/starters.jpg"
   },
-  // ...continue for all categories
+  {
+    name: "German Doner",
+    image: "https://example.com/images/germandoner.jpg"
+  },
+  {
+    name: "Kebabs",
+    image: "https://example.com/images/kebabs.jpg"
+  },
+  {
+    name: "Combo Kebabs",
+    image: "https://example.com/images/combo.jpg"
+  },
+  {
+    name: "Pizzas",
+    image: "https://example.com/images/pizzas.jpg"
+  },
+  {
+    name: "Create Your Own Pizza",
+    image: "https://example.com/images/createpizza.jpg"
+  },
+  {
+    name: "Garlic Pizzas",
+    image: "https://example.com/images/garlicpizza.jpg"
+  },
+  {
+    name: "Wraps",
+    image: "https://example.com/images/wraps.jpg"
+  },
+  {
+    name: "Burgers",
+    image: "https://example.com/images/burgers.jpg"
+  },
+  {
+    name: "Vegetarian",
+    image: "https://example.com/images/vegetarian.jpg"
+  },
+  {
+    name: "Meal Deals",
+    image: "https://example.com/images/mealdeals.jpg"
+  },
+  {
+    name: "Kids",
+    image: "https://example.com/images/kids.jpg"
+  },
+  {
+    name: "Extras",
+    image: "https://example.com/images/extras.jpg"
+  },
+  {
+    name: "Sauces",
+    image: "https://example.com/images/sauces.jpg"
+  },
+  {
+    name: "Desserts",
+    image: "https://example.com/images/desserts.jpg"
+  },
+  {
+    name: "Drinks",
+    image: "https://example.com/images/drinks.jpg"
+  }
 ];
-
-export default menu;
