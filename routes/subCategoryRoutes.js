cb67ea3fbcf5e2360966f6df33dd7c413eaf1597
ethanparker3512 <ethanparker3512@gmail.com>
2025-11-18 23:48:32 +0100

    Removed missing createSubCategory import to fix Render deploy

--- routes/subCategoryRoutes.js
+++ routes/subCategoryRoutes.js
@@ -1,5 +1,8 @@
 import express from "express";
-import { getAllSubCategories, getSubCategoriesByCategory } from "../controllers/subCategoryController.js";
+import {
+  getAllSubCategories,
+  getSubCategoriesByCategory
+} from "../controllers/subCategoryController.js";
 
 const router = express.Router();
 
