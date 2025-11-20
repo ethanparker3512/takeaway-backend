855d2b525dfc5dd553268d6d6dd71409402c90bb
ethanparker3512 <ethanparker3512@gmail.com>
2025-11-20 15:51:30 +0100

    Added food controller and fixed /api/foods route

--- routes/foodRoutes.js
+++ routes/foodRoutes.js
@@ -1,56 +1,9 @@
 import express from "express";
-import Category from "../models/Category.js";
-import SubCategory from "../models/SubCategory.js";
-import Food from "../models/Food.js";
+import { getFoods } from "../controllers/foodController.js";
 
 const router = express.Router();
 
-/*
-|--------------------------------------------------------------------------
-| GET ALL CATEGORIES
-|--------------------------------------------------------------------------
-*/
-router.get("/categories", async (req, res) => {
-  try {
-    const categories = await Category.find();
-    res.json(categories);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
-
-/*
-|--------------------------------------------------------------------------
-| GET SUBCATEGORIES BY CATEGORY ID
-|--------------------------------------------------------------------------
-*/
-router.get("/categories/:id/subcategories", async (req, res) => {
-  try {
-    const subcategories = await SubCategory.find({
-      category: req.params.id,
-    });
-
-    res.json(subcategories);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
-
-/*
-|--------------------------------------------------------------------------
-| GET FOODS BY SUBCATEGORY ID
-|--------------------------------------------------------------------------
-*/
-router.get("/subcategories/:id/foods", async (req, res) => {
-  try {
-    const foods = await Food.find({
-      subcategory: req.params.id,
-    });
-
-    res.json(foods);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
+// GET all foods
+router.get("/foods", getFoods);
 
 export default router;
