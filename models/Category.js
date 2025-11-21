0b8cf6d018fb7f893a3341a487f4452aa8d9b8dc
ethanparker3512 <ethanparker3512@gmail.com>
2025-11-21 03:50:34 +0100

    Category.js

--- models/Category.js
+++ models/Category.js
@@ -3,7 +3,8 @@ import mongoose from "mongoose";
 const CategorySchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
-    image: { type: String }
+    image: { type: String, required: true },
+    price: { type: Number, required: true }
   },
   { timestamps: true }
 );
