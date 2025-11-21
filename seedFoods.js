ec695a3e26e16ee200512996081b6147d33400f2
ethanparker3512 <ethanparker3512@gmail.com>
2025-11-21 05:10:14 +0100

    seedFoods.js

--- seedFoods.js
+++ seedFoods.js
@@ -3,45 +3,24 @@ import SubCategory from "./models/SubCategory.js";
 
 export const seedFoods = async (req, res) => {
   try {
-    // Clear existing data
     await Category.deleteMany({});
     await SubCategory.deleteMany({});
 
-    // Insert Categories
     const categories = await Category.insertMany([
-      {
-        name: "Rice Dishes",
-        image: "https://i.imgur.com/riceCategory.png",
-        price: 25,
-      },
-      {
-        name: "Pizza",
-        image: "https://i.imgur.com/pizzaCategory.png",
-        price: 50,
-      },
-      {
-        name: "Drinks",
-        image: "https://i.imgur.com/drinkCategory.png",
-        price: 3,
-      },
+      { name: "Rice Dishes", image: "https://i.imgur.com/riceCategory.png", price: 25 },
+      { name: "Pizza", image: "https://i.imgur.com/pizzaCategory.png", price: 50 },
+      { name: "Drinks", image: "https://i.imgur.com/drinkCategory.png", price: 3 }
     ]);
 
-    // Map IDs
     const riceCategory = categories.find(c => c.name === "Rice Dishes")._id;
     const pizzaCategory = categories.find(c => c.name === "Pizza")._id;
     const drinksCategory = categories.find(c => c.name === "Drinks")._id;
 
-    // Insert SubCategories
     await SubCategory.insertMany([
-      // Rice
       { name: "Jollof Rice + Chicken", price: 25, category: riceCategory },
       { name: "Fried Rice + Turkey", price: 30, category: riceCategory },
-
-      // Pizza
       { name: "Pepperoni Pizza Medium", price: 50, category: pizzaCategory },
-
-      // Drinks
-      { name: "Coke 50cl", price: 3, category: drinksCategory },
+      { name: "Coke 50cl", price: 3, category: drinksCategory }
     ]);
 
     res.json({ message: "Seed successful!" });
