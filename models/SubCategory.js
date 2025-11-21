9f7bb89e2f287cd85ac153825d722ea7a24e767b
ethanparker3512 <ethanparker3512@gmail.com>
2025-11-21 05:09:35 +0100

    SubCategory.js

--- models/SubCategory.js
+++ models/SubCategory.js
@@ -1,16 +1,9 @@
 import mongoose from "mongoose";
 
-const subCategorySchema = new mongoose.Schema(
-  {
-    name: { type: String, required: true },
-    price: { type: Number, required: true },
-    category: { 
-      type: mongoose.Schema.Types.ObjectId, 
-      ref: "Category",
-      required: true 
-    }
-  },
-  { timestamps: true }
-);
+const SubCategorySchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  price: { type: Number, required: true },
+  category: { type: mongoose.Schema.Types.ObjectId, ref: "Category", required: true }
+}, { timestamps: true });
 
-export default mongoose.model("SubCategory", subCategorySchema);
+export default mongoose.models.SubCategory || mongoose.model("SubCategory", SubCategorySchema);
