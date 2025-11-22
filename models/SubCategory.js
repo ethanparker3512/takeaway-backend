ff7404d05aad113bffefbcc9bcbb5b654f2ae4f1
ethanparker3512 <ethanparker3512@gmail.com>
2025-11-22 06:54:47 +0100

    SubCategory.js

--- models/SubCategory.js
+++ models/SubCategory.js
@@ -1,9 +1,19 @@
 import mongoose from "mongoose";
 
-const SubCategorySchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  price: { type: Number, required: true },
-  category: { type: mongoose.Schema.Types.ObjectId, ref: "Category", required: true }
-}, { timestamps: true });
+const SubCategorySchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    price: { type: Number, required: true },
+    category: { type: mongoose.Schema.Types.ObjectId, ref: "Category", required: true },
+  },
+  { timestamps: true, toJSON: { virtuals: true }, toObject: { virtuals: true } }
+);
+
+// Virtual for foods
+SubCategorySchema.virtual("foods", {
+  ref: "Food",
+  localField: "_id",
+  foreignField: "subcategory",
+});
 
 export default mongoose.models.SubCategory || mongoose.model("SubCategory", SubCategorySchema);
