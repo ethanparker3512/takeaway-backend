245c0c6623b32fe3284b9c74c2f974b2b2131bf2
ethanparker3512 <ethanparker3512@gmail.com>
2025-11-22 04:30:46 +0100

    seedRoutes.js

--- routes/seedRoutes.js
+++ routes/seedRoutes.js
@@ -1,8 +1,15 @@
-import express from "express";
+\import express from "express";
 import { seedFoods } from "../seedFoods.js";
 
 const router = express.Router();
 
-router.post("/seed/foods", seedFoods);
+router.get("/", async (req, res) => {
+  try {
+    await seedFoods();
+    res.json({ message: "Seeding complete" });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
 
 export default router;
