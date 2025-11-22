729ba441eceea6f761fd3f333c794fcec924b658
ethanparker3512 <ethanparker3512@gmail.com>
2025-11-22 18:38:09 +0100

    subCategoryRoutes.js

--- routes/subCategoryRoutes.js
+++ routes/subCategoryRoutes.js
@@ -1,9 +1,12 @@
 import express from "express";
-import { createSubCategory, getSubCategoriesByCategory } from "../controllers/subCategoryController.js";
+import { subCategoryList } from "../subCategoryList.js";
 
 const router = express.Router();
 
-router.post("/", createSubCategory);
-router.get("/:categoryId", getSubCategoriesByCategory);
+router.get("/:categoryId", (req, res) => {
+  const id = parseInt(req.params.categoryId);
+  const filtered = subCategoryList.filter(item => item.categoryId === id);
+  res.json(filtered);
+});
 
 export default router;
