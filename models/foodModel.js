d896cdc2e26ea566dbed16044088512843a717d7
ethanparker3512 <ethanparker3512@gmail.com>
2025-11-22 07:03:54 +0100

    foodModel.js

--- models/foodModel.js
+++ models/foodModel.js
@@ -2,31 +2,12 @@ import mongoose from "mongoose";
 
 const foodSchema = new mongoose.Schema(
   {
-    name: {
-      type: String,
-      required: true,
-    },
-
-    price: {
-      type: Number,
-      required: true,
-    },
-
-    image: {
-      type: String,
-    },
-
-    description: {
-      type: String,
-    },
-
-    subcategory: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "SubCategory",
-      required: true,
-    },
+    name: { type: String, required: true },
+    price: { type: Number, required: true },
+    image: { type: String, required: true },
+    subcategory: { type: mongoose.Schema.Types.ObjectId, ref: "SubCategory", required: true },
   },
   { timestamps: true }
 );
 
-export default mongoose.models.Food || mongoose.model("Food", foodSchema);
+export default mongoose.model("Food", foodSchema);
